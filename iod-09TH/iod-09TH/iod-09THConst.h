54f1c722c0855d643b2b670d936e65e041952c58
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2021-10-25 23:43:49 +0200

    Update screen

--- iod-09TH/iod-09TH/iod-09THConst.h
+++ iod-09TH/iod-09TH/iod-09THConst.h
@@ -1,14 +1,14 @@
-// File generated 21/09/30 21:46:47
+// File generated 21/10/25 15:57:36
 // Warning! This is a generated file, any manual changes will be
 // lost during the next generation.
 
 #define  IFONT_OFFSET     0
 // object indexes into ImageControl
 #define iStatictext1            0 // offset 0x0
-#define iStatictext2            1 // offset 0xE00
-#define iStatictext3            2 // offset 0x1400
-#define iStatictext4            3 // offset 0x1A00
-#define iStatictext5            4 // offset 0x1E00
+#define iStatictext3            1 // offset 0x200
+#define iStatictext5            2 // offset 0x800
+#define iStatictext6            3 // offset 0xE00
+#define iStatictext7            4 // offset 0x1800
 
 
 static const bool inputs[] PROGMEM = { false, false, false, false, false} ;
